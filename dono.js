3a71ba8d2d2fd949c0d66f6f26f1340e8fd31a1e
hgoliveiraa <125594395+hgoliveiraa@users.noreply.github.com>
2023-02-19 00:15:21 -0300

    Add files via upload

---
+++ dono.js
@@ -0,0 +1,12 @@
+{
+	"pr": ".",
+	"donoofc": "5521964090961",
+	"dono2": "dono-2",
+	"dono3": ".",
+	"dono4": ".",
+	"dono5": ".",
+	"dono6": ".",
+	"dono7": ".",
+	"nomeDono": "eu hg",
+	"NomeDoBot": "™𝕭𝖔𝖙"
+}
